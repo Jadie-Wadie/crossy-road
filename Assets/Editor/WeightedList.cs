7cfcbc4533f7079baeb0504146843ac98e722b25
Jadie Wadie <jaidentdouglas@gmail.com>
2020-06-24 18:11:21 +0930

    split Editor scripts

---
+++ Assets/Editor/WeightedList.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(Weight))]
+public class ColorPointDrawer : PropertyDrawer
+{
+	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+	{
+		label = EditorGUI.BeginProperty(position, label, property);
+
+		Rect contentPosition = EditorGUI.PrefixLabel(position, label);
+		contentPosition.width *= 0.5f;
+		EditorGUI.indentLevel = 0;
+
+		EditorGUI.PropertyField(contentPosition, property.FindPropertyRelative("weight"), GUIContent.none);
+		contentPosition.x += contentPosition.width;
+		EditorGUI.PropertyField(contentPosition, property.FindPropertyRelative("prefab"), GUIContent.none);
+
+		EditorGUI.EndProperty();
+	}
+}
--- Assets/Scripts/Control/WeightedList.cs
+++ Assets/Scripts/Control/WeightedList.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
@@ -8,25 +8,6 @@ public struct Weight
 {
 	public int weight;
 	public GameObject prefab;
-}
-
-[CustomPropertyDrawer(typeof(Weight))]
-public class ColorPointDrawer : PropertyDrawer
-{
-	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
-	{
-		label = EditorGUI.BeginProperty(position, label, property);
-
-		Rect contentPosition = EditorGUI.PrefixLabel(position, label);
-		contentPosition.width *= 0.5f;
-		EditorGUI.indentLevel = 0;
-
-		EditorGUI.PropertyField(contentPosition, property.FindPropertyRelative("weight"), GUIContent.none);
-		contentPosition.x += contentPosition.width;
-		EditorGUI.PropertyField(contentPosition, property.FindPropertyRelative("prefab"), GUIContent.none);
-
-		EditorGUI.EndProperty();
-	}
 }
 
 [System.Serializable]
